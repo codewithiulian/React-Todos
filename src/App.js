7df1f8015b6a60cb4b193f925b7af8fa1f11ab88
Iulian Oana <iulian.andreioana@gmail.com>
2020-10-14 12:06:47 +0100

    Setting up the initial project components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Todos from './components/Todos';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      todos: [
+        {
+          id: 1,
+          title: 'Walk the dog',
+          completed: false
+        },
+        {
+          id: 2,
+          title: 'Do the dishes',
+          completed: false
+        },
+        {
+          id: 3,
+          title: 'Think about your future',
+          completed: false
+        },
+      ]
+    };
+  }
+
+  render() {
+    console.log(this.state);
+    return (
+      <div className="App">
+        <Todos todos={this.state.todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
